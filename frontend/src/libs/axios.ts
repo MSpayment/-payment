7630ee69fd7a82d25d4bdc590c79cf565aaec944
Shota-Kurahashi <Myheroes0715@gmail.com>
2023-03-03 16:33:19 +0900

    add refresh token

--- frontend/src/libs/axios.ts
+++ frontend/src/libs/axios.ts
@@ -1,8 +1,28 @@
+/* eslint-disable no-underscore-dangle */
 import _axios from "axios";
 
 const axios = _axios.create({
   baseURL: process.env.NEXT_PUBLIC_ENDPOINT,
   withCredentials: true,
 });
+
+axios.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        await axios.post("/auth/refresh");
+
+        return axios(originalRequest);
+      } catch (err) {
+        throw new Error("ログインしてください");
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
 
 export default axios;

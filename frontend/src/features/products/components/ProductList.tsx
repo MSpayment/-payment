3ff51371df2329fa0d53440188e3c9ef3c7b9e44
Shota-Kurahashi <Myheroes0715@gmail.com>
2023-03-08 16:46:17 +0900

    add month

--- frontend/src/features/products/components/ProductList.tsx
+++ frontend/src/features/products/components/ProductList.tsx
@@ -11,7 +11,7 @@ type Props = {
 export const ProductList: FC<Props> = ({ productList }) => (
   <li className="border-0 border-b border-solid border-slate-200 py-2">
     <Text underline className="sticky top-0 bg-white">
-      {format(new Date(productList?.date), "yyyy年MM月dd日")}
+      {productList && format(new Date(productList?.date), "yyyy年MM月dd日")}
     </Text>
     <ul className="mt-2 space-y-4">
       {productList?.products?.map((product) => (
--- frontend/src/features/products/components/Products.tsx
+++ frontend/src/features/products/components/Products.tsx
@@ -1,13 +1,16 @@
-import { Modal } from "@mantine/core";
+import { Modal, Text } from "@mantine/core";
 import React, { FC } from "react";
 import { ModalContent } from "src/components/ModalContent";
 import { useQueryProducts } from "src/features/products/api/useQueryProducts";
 import { ProductList } from "src/features/products/components/ProductList";
+import { useSelectDateState } from "src/features/products/store";
 
 import { useGlobalState } from "src/store/input";
 
 export const Products: FC = () => {
-  const { data } = useQueryProducts();
+  const selectDate = useSelectDateState((state) => state.date);
+
+  const { data } = useQueryProducts(selectDate.year, selectDate.month);
   const modal = useGlobalState((state) => state.modal);
   const setModal = useGlobalState((state) => state.setModal);
 
@@ -26,11 +29,22 @@ export const Products: FC = () => {
       >
         <ModalContent />
       </Modal>
-      <ul className="p-4">
-        {data?.map((productList) => (
-          <ProductList productList={productList} key={productList?.id} />
-        ))}
-      </ul>
+      {data?.length === 0 ? (
+        <div>
+          <Text
+            component="p"
+            className="pt-4 text-center text-2xl font-bold text-blue-500"
+          >
+            まだ何も登録されていません
+          </Text>
+        </div>
+      ) : (
+        <ul className="p-4">
+          {data?.map((productList) => (
+            <ProductList productList={productList} key={productList?.id} />
+          ))}
+        </ul>
+      )}
     </>
   );
 };

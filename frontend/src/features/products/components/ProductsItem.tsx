86b506a5a9c3c27d8b1f70ec7b04c0d94913640e
makim0939 <95673492+makim0939@users.noreply.github.com>
2023-03-15 09:05:08 +0900

    支払い済みの見た目を変えた

--- frontend/src/features/products/components/ProductsItem.tsx
+++ frontend/src/features/products/components/ProductsItem.tsx
@@ -32,39 +32,54 @@ export const ProductsItem: FC<Props> = ({ product }) => {
 
   return (
     <li className="flex w-full flex-col">
-      <Badge
-        size="xs"
-        className="mb-3 max-w-max"
-        color={product.boughtSite === "メルカリ" ? "red" : "blue"}
-      >
-        {product.boughtSite}
-      </Badge>
+      <div style={product.isPaid ? { color: "#ccc" } : { color: "#222" }}>
+        <Badge
+          size="xs"
+          className="mb-3 max-w-max"
+          // color= {product.isPaid ? "gray" : (product.boughtSite === "メルカリ" ? "red" : "blue")}
+          color={(() => {
+            if (product.isPaid) return "gray.4";
+            if (product.boughtSite === "メルカリ") {
+              return "red";
+            }
 
-      <div className="flex gap-2">
-        <Text
-          component="p"
-          className="flex-1 overflow-hidden break-words text-sm"
+            return "blue";
+          })()}
         >
-          {product.name}
-        </Text>
-        <Text component="p" className="w-24 text-right text-sm">
-          {product.price.toLocaleString()}円
-        </Text>
-        <div className="flex justify-center space-x-2">
-          {updateProductMutation.isLoading ? (
-            <Loader size="xs" />
-          ) : (
-            <Checkbox onChange={handleIsPaidChange} checked={product.isPaid} />
-          )}
-          {deleteProductMutation.isLoading ? (
-            <Loader size="xs" color="red" />
-          ) : (
-            <Checkbox
-              onChange={deleteProductHandler}
-              color="red"
-              checked={product.deleted}
-            />
-          )}
+          {product.boughtSite}
+        </Badge>
+        <div className="flex gap-2">
+          <Text
+            component="p"
+            className="flex-1 overflow-hidden break-words text-sm"
+            color={product.boughtSite ? "black" : "blue"}
+            style={product.isPaid ? { color: "#d0d0d0" } : { color: "#222" }}
+          >
+            {product.name}
+          </Text>
+          <Text component="p" className="w-24 text-right text-sm">
+            {product.price.toLocaleString()}円
+          </Text>
+          <div className="flex justify-center space-x-2">
+            {updateProductMutation.isLoading ? (
+              <Loader size="xs" />
+            ) : (
+              <Checkbox
+                onChange={handleIsPaidChange}
+                color={product.isPaid ? "gray.4" : "blue"}
+                checked={product.isPaid}
+              />
+            )}
+            {deleteProductMutation.isLoading ? (
+              <Loader size="xs" color="red" />
+            ) : (
+              <Checkbox
+                onChange={deleteProductHandler}
+                color={product.isPaid ? "gray.4" : "red"}
+                checked={product.deleted}
+              />
+            )}
+          </div>
         </div>
       </div>
     </li>
